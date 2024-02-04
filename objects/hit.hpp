0c9a586ce0973f7b43a9ec13000ade69c3bd189c
Karma Riuk <riukkarma@gmail.com>
2024-02-04 15:14:23 +0100

    hit now considers front faces

--- objects/hit.hpp
+++ objects/hit.hpp
@@ -1,9 +1,18 @@
 #pragma once
 
+#include "../ray.hpp"
 #include "../vec3.hpp"
 
 struct hit {
-    bool hit;
-    point3 intersection;
+    double t;
+    bool hit = false;
+    point3 p;
     vec3 normal;
+
+    bool front_face;
+
+    void set_face_normal(const ray& r, const vec3& outward_normal) {
+        front_face = dot(r.direction(), outward_normal) < 0;
+        normal = front_face ? outward_normal : -outward_normal;
+    }
 };
