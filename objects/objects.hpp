6d5c405bfdafb309fcb723760ea073e3cd25c8d1
Karma Riuk <riukkarma@gmail.com>
2024-02-04 15:17:01 +0100

    created the object list (a composite of objects)

--- objects/objects.hpp
+++ objects/objects.hpp
@@ -3,7 +3,39 @@
 #include "../ray.hpp"
 #include "hit.hpp"
 
+#include <memory>
+#include <vector>
+
 class object {
   public:
     virtual hit intersect(const ray& r, double t_min, double t_max) const = 0;
+};
+
+class object_list : object {
+  public:
+    std::vector<std::shared_ptr<object>> objects;
+
+    void clear() {
+        objects.clear();
+    }
+
+    void add(std::shared_ptr<object> obj) {
+        objects.push_back(obj);
+    }
+
+    hit intersect(const ray& r, double t_min, double t_max) const override {
+        hit tmp_hit;
+        hit hit;
+        auto closest_so_far = t_max;
+
+        for (const auto& obj : objects) {
+            tmp_hit = obj->intersect(r, t_min, closest_so_far);
+            if (tmp_hit.hit) {
+                closest_so_far = tmp_hit.t;
+                hit = tmp_hit;
+            }
+        }
+
+        return hit;
+    }
 };
