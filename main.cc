ac1343daacf3725c76ee867e3278c524ed0f67cf
Karma Riuk <riukkarma@gmail.com>
2024-02-17 01:43:55 +0100

    brought back the old random scene

--- main.cc
+++ main.cc
@@ -10,76 +10,68 @@
 int main() {
     object_list scene;
 
-    vec3 earth_center = vec3(0, -1000, 0);
-    double earth_radius = 1000;
-    auto material_ground = make_shared<lambertian>(color(.5, .5, .5));
-    scene.add(
-        std::make_shared<sphere>(earth_center, earth_radius, material_ground)
-    );
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    scene.add(make_shared<sphere>(point3(0, -1000, 0), 1000, ground_material));
+
     for (int a = -11; a < 11; a++) {
         for (int b = -11; b < 11; b++) {
-            double choose_mat = random_double();
-            double radius = random_double(1, 5);
+            auto choose_mat = random_double();
             point3 center(
-                10 * a + 10 * random_double(),
-                radius,
-                10 * b + 10 * random_double()
+                a + 0.9 * random_double(),
+                0.2,
+                b + 0.9 * random_double()
             );
 
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
 
-            vec3 E2C = center - earth_center;
-            center = earth_center + (earth_radius * unit_vector(E2C))
-                   + radius * unit_vector(E2C);
-
-            shared_ptr<material> sphere_material;
-            if (choose_mat < 0.8) {
-                // Diffuse
-                color albedo = color::random() * color::random();
-                sphere_material = make_shared<lambertian>(albedo);
-                scene.add(
-                    std::make_shared<sphere>(center, radius, sphere_material)
-                );
-            } else if (choose_mat < 0.95) {
-                // Metal
-                color albedo = color::random(.5, 1);
-                double fuzz = random_double(0, .5);
-                sphere_material = make_shared<metal>(albedo, fuzz);
-                scene.add(
-                    std::make_shared<sphere>(center, radius, sphere_material)
-                );
-            } else {
-                // Glass
-                sphere_material = make_shared<dielectric>(1.5);
-                scene.add(
-                    std::make_shared<sphere>(center, radius, sphere_material)
-                );
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    scene.add(make_shared<sphere>(center, 0.2, sphere_material)
+                    );
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    scene.add(make_shared<sphere>(center, 0.2, sphere_material)
+                    );
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    scene.add(make_shared<sphere>(center, 0.2, sphere_material)
+                    );
+                }
             }
         }
     }
 
     auto material1 = make_shared<dielectric>(1.5);
-    auto material2 = make_shared<lambertian>(color(.4, .2, .1));
-    auto material3 = make_shared<metal>(color(.7, .6, .9), 0.0);
+    scene.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
 
-    scene.add(std::make_shared<sphere>(point3(0, 1, 0), 1, material1));
-    scene.add(std::make_shared<sphere>(point3(-4, 1, 0), 1, material2));
-    scene.add(std::make_shared<sphere>(point3(4, 1, 0), 1, material3));
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    scene.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    scene.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
 
     scene = object_list(make_shared<bvh_node>(scene));
 
     camera cam;
     cam.aspect_ratio = 9. / 16.;
-    cam.image_width = 800;
-    cam.samples_per_pixel = 50;
+    cam.image_width = 1200;
+    cam.samples_per_pixel = 32;
     cam.max_depth = 8;
 
     cam.vfov = 20;
-
-    cam.lookfrom = point3(130, 50, 80);
+    cam.lookfrom = point3(13, 2, 3);
     cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
 
     cam.defocus_angle = 0.6;
-    cam.focus_dist = 150;
+    cam.focus_dist = 10.0;
 
     cam.render(scene);
 }
